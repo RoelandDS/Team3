003e2a4c78d422aa86d0781b55665671df7a5078
Roelandds <josip.koninckx@student.pxl.be>
2015-12-03 14:10:23 +0100

    Ridding of useless files

--- htdocs/server.js
+++ htdocs/server.js
@@ -10,8 +10,8 @@ var teamnaam = {
     ]
 };
 
-app.use(express.static(__dirname));
 
+app.use(express.static(__dirname));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
