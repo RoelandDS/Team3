52c05d0378bc0a8759a80b3e1abbde4395c98867
GarrusVakarian <ben.elen@student.pxl.be>
2015-12-03 14:11:45 +0100

    Added mysql image retrieval

--- htdocs/server.js
+++ htdocs/server.js
@@ -1,14 +1,23 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 
 // JSON retour zenden
 var teamnaam = {
     name: "FoxPaw Hacking",
     members: [
-        { name: "Ben Elen" },
-        { name: "Josip Koninckx" }
+        {name: "Ben Elen"},
+        {name: "Josip Koninckx"}
     ]
 };
+
+var connection = mysql.createConnection({
+    host: '173.194.105.180',
+    port: '3306',
+    user: 'student',
+    password: 'mulestudent',
+    database: 'training'
+});
 
 app.use(express.static(__dirname));
 
@@ -23,6 +32,19 @@ app.get('/', function (req, res) {
     res.json(teamnaam);
 });
 
+app.get('/qr', function (req, res) {
+    connection.connect();
 
+    connection.query('SELECT * from qr_code WHERE img_title LIKE "%FoxPaw Hacking%"', function (err, rows, fields) {
+        if (!err & rows.length > 0)
+            res.json(rows[0]);
+        else {
+            res.status(404);
+            res.send({ error: 'Not found' });
+        }
+    });
+
+    connection.end();
+});
 
 app.listen(4000);
