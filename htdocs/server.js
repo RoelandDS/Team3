e7dc42315f8dff6a4f337becf37750fc55c57129
Roelandds <josip.koninckx@student.pxl.be>
2015-12-03 12:55:31 +0100

    do it

--- htdocs/server.js
+++ htdocs/server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+// 2. JSON retour zenden
+var teamnaam = {
+    name: "FoxPaw Hacking",
+    members: [
+        { name: "Ben Elen" },
+        { name: "Josip Koninckx" }
+    ]
+};
+
 // 1. Routes voor deze app - ten eerste de Homepage
 app.get('/', function (req, res) {
     res.writeHead(200, {'Content-Type' : 'application/json'});
@@ -12,14 +21,7 @@ app.get('/home', function (req, res) {
     res.send();
 });
 
-// 2. JSON retour zenden
-var teamnaam = {
-    name: "FoxPaw Hacking",
-    members: [
-        { name: "Ben Elen" },
-        { name: "Josip Koninckx" }
-    ]
-};
+
 
 app.listen(4000);
 console.log('Express-server gestart op http://localhost:3000');
